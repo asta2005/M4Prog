7f11cce373e95097bf08e38497ac482e3b69123e
Asta <omaralinora.1@gmail.com>
2024-05-14 12:51:26 +0200

    huisdieren

---
+++ dockerM4Prog/sql/02data/Huisdierdata.sql
@@ -0,0 +1,9 @@
+INSERT INTO `Huisdieren` (`id`, `naam`, `eigenaar`, `typedier`, `leeftijd`, `geboortedatum`)
+ VALUES
+ 
+  (NULL, 'ravikie', 'omar kahouach', 'papegaai', '4', '2020-05-13'),
+  (NULL, 'rayan', 'omar ', 'rat', '4', '2022-05-13'),
+  (NULL, 'komsti', 'swemdie', 'kikker', '5', '1999-05-13'),
+  (NULL, 'rabi', 'misata', 'konijn', '3', '2024-05-13'),
+  (NULL, 'yuno', 'omar', 'koi vis', '1', '2020-05-13'),
+  (NULL, 'liera', 'asta', 'schieldpad', '2', '1989-05-13');
